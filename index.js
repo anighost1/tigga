44cecc64979e1f7873713d12de738a1ae6069b2b
Anil Tigga <anil.anil300@gmail.com>
2024-09-21 01:48:51 +0530

    microservice setup

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+/* eslint-disable no-undef */
 import express from 'express';
 import cors from 'cors'
 import mongoConnect from './config/mongodb.config.js';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import routeProtection from './middleware/routeProtection.js';
 
 import authRoute from './routes/auth.route.js'
 
 const app = express();
-// eslint-disable-next-line no-undef
 const port = process.env.PORT || 1998;
+const microservices = JSON.parse(process.env.MICROSERVICES || '{}');
 
 mongoConnect()
 
@@ -18,7 +21,28 @@ app.get('/', (req, res) => {
     res.send('Tigga');
 })
 
-app.use('/api/auth',authRoute)
+app.use('/api/auth', authRoute)
+
+app.get('/api/services', routeProtection, (req, res) => {
+    res.json(microservices);
+});
+
+app.use('/api/:service', routeProtection, (req, res, next) => {
+    const serviceName = req.params.service;
+    const serviceUrl = microservices[serviceName];
+
+    if (serviceUrl) {
+        createProxyMiddleware({
+            target: serviceUrl,
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/api/${serviceName}`]: '',
+            },
+        })(req, res, next);
+    } else {
+        res.status(404).json({ error: `Service '${serviceName}' not found` });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Tigga server running at port : ${port}`);
